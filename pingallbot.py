bd6d131138c348e677239c80ee02520d83bdfdfe
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2022-09-03 20:52:26 +0530

    Update pingallbot.py

--- pingallbot.py
+++ pingallbot.py
@@ -232,20 +232,23 @@ async def bots(client, message):
 
 @teletips.on_message(filters.command("start") & filters.private)
 async def start(client, message):   
-    text = f'''Hi!! {message.from_user.mention},
+    txt = f'''Hi!! {message.from_user.mention},
 **I'm Miku A Simple Mention All Bot.** I can help you to get everyone's attention by mentioning all members in your chat.
 I have some additional cool features and also I can work in channels.
 
 📜 __Click__ **/help** __for more information__
 '''
-    link = InlineKeyboardMarkup([
+    link = InlineKeyboardMarkup(
+            [
                 [
                     InlineKeyboardButton(
-                        text="➕ Add me to your group",
-                        url= f"https://telegram.me/MikuMentionAll_bot?startgroup=true",
-                        )
-                ]])    
-    await teletips.send_message(message.chat.id, text, link)
+                        "➕Add me to your Group.",
+                        url = f"https://telegram.me/RequestTracker_bot?startgroup=true"
+                    )
+                ]
+            ]
+        )
+    await teletips.send_message(message.chat.id, txt, link)
 
 
 @teletips.on_message(filters.command("help"))
