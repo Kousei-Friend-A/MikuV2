ce9fd7500c8b1d70ff2413f962ee768d8a7a8b9b
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2022-09-03 21:14:25 +0530

    Update pingallbot.py

--- pingallbot.py
+++ pingallbot.py
@@ -226,7 +226,7 @@ async def bots(client, message):
 
 @teletips.on_message(filters.command("start") & filters.private)
 async def start(client, message):   
-    txt = f'''Hi!! {message.from_user.mention},
+    text = f'''Hi!! {message.from_user.mention},
 **I'm Miku A Simple Mention All Bot.** 
 
 I can help you to get everyone's attention by mentioning all members in your chat.
